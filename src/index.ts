f9615d079b12922e74ce4529900775b38e37f895
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-26 16:35:05 -0500

    add new api endpoints

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,12 @@ import dotenv from "dotenv";
 import connectDB from "./db";
 import userRoutes from "./routes/user";
 import authRoutes from "./routes/auth";
+import categoryRoutes from "./routes/categories";
+import productRoutes from "./routes/products";
+import imageRouter from "./routes/image";
 
 dotenv.config();
+const { PORT } = process.env;
 
 const app = express();
 app.use(express.json());
@@ -22,10 +26,12 @@ app.use(
 
 connectDB();
 
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/auth", authRoutes); // -> /auth/signin | /auth/signup
+app.use("/api", userRoutes); // -> /api/users | /api/user | /api/user/:id
+app.use("/api", categoryRoutes); // -> /api/categories | /api/category | /api/category/:id
+app.use("/api", productRoutes); // -> /api/products | /api/product | /api/product/:id
+app.use("/api/image", imageRouter); // -> /api/image/upload | /api/image/remove
 
-const { PORT } = process.env;
 const APP_PORT = PORT || 5000;
 app.listen(APP_PORT, () => {
   console.log(`Server: Active | Port: ${APP_PORT}.`);
