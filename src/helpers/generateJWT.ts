4a1b946833f13492a27a5270b02ea90020eb16b0
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-26 16:34:16 -0500

    rename JWT generate helper

--- src/helpers/generateJWT.ts
+++ src/helpers/generateJWT.ts
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 const { ACCESS_TOKEN_SECRET_SEED } = process.env;
 
-export const jwtGenerate = (uid: string) => {
+export const generateJWT = (uid: string) => {
   return new Promise((resolve, reject) => {
     const payload = { uid };
     jwt.sign(
       payload,
       String(ACCESS_TOKEN_SECRET_SEED),
       {
-        expiresIn: "4h",
+        expiresIn: "24h",
       },
       (err, token) => {
         if (err) {
