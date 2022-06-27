b4bfc819fbe94d95ba79ac26482437b647f45c80
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-27 09:57:43 -0500

    delete folders

--- src/models/index.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "./user";
-import { Category } from "./category";
-import { Payment } from "./payment";
-import { Product } from "./product";
-
-export { User, Category, Payment, Product };
--- src/routes/image.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { imageController } from "../controllers";
-
-const router = express.Router();
-
-router.route("/upload").post(imageController.uploadImage);
-router.route("/remove").post(imageController.removeImage);
-
-export default router;
