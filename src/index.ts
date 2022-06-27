1456bd31ae23aef7822a31b8203a602100258150
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-27 09:57:26 -0500

    change controllers routes

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,16 @@ import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
 
 import connectDB from "./db";
-import userRoutes from "./routes/user";
-import authRoutes from "./routes/auth";
-import categoryRoutes from "./routes/categories";
-import productRoutes from "./routes/products";
-import imageRouter from "./routes/image";
+
+import userRoute from "./api/users";
+import authRoutes from "./api/auth";
+import categoryRoutes from "./api/categories";
+import productRoutes from "./api/products";
+import imageRoutes from "./api/images";
+import paymentRoutes from "./api/payments";
 
 dotenv.config();
-const { PORT } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 const app = express();
 app.use(express.json());
@@ -27,14 +29,16 @@ app.use(
 connectDB();
 
 app.use("/auth", authRoutes); // -> /auth/signin | /auth/signup
-app.use("/api", userRoutes); // -> /api/users | /api/user | /api/user/:id
+app.use("/api", userRoute); // -> /api/users | /api/user | /api/user/:id
 app.use("/api", categoryRoutes); // -> /api/categories | /api/category | /api/category/:id
 app.use("/api", productRoutes); // -> /api/products | /api/product | /api/product/:id
-app.use("/api/image", imageRouter); // -> /api/image/upload | /api/image/remove
+app.use("/api/image", imageRoutes); // -> /api/image/upload | /api/image/remove
+app.use("/api/payments", paymentRoutes); // -> /api/payment/
 
 const APP_PORT = PORT || 5000;
 app.listen(APP_PORT, () => {
-  console.log(`Server: Active | Port: ${APP_PORT}.`);
+  NODE_ENV === "development" && console.log(`Port: ${APP_PORT}.`);
+  NODE_ENV === "development" && console.log("Server: Active.");
 });
 
 export { app };
